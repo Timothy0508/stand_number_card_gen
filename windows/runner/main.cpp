b4b0becbdf8bdfb8cde5588f62a8f9f78ae627f8
Timothy0508 <99249271+Timothy0508@users.noreply.github.com>
2025-03-29 17:13:54 +0800

    Rename application window title to "Number Card Generator"

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"stand_number_card_gen", origin, size)) {
+  if (!window.Create(L"Number Card Generator", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
